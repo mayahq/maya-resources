45ee3e32db15e93f5415c44288af704b09c39324
Dushyant Yadav <dushyant9309@gmail.com>
2022-12-23 20:57:15 +0530

    Remove api suffix from pac base URL

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,8 @@
 let MAYA_PAC_URL = 'http://localhost:8000'
 if (process.env.RUNTIME_ENVIRONMENT === 'PRODUCTION') {
-    MAYA_PAC_URL = 'https://api.mayalabs.io/pac/api'
+    MAYA_PAC_URL = 'https://api.mayalabs.io/pac'
 } else if (process.env.RUNTIME_ENVIRONMENT === 'STAGING') {
-    MAYA_PAC_URL = 'https://api.dev.mayalabs.io/pac/api'
+    MAYA_PAC_URL = 'https://api.dev.mayalabs.io/pac'
 }
 
 module.exports = {
