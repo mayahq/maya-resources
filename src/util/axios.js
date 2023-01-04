8a75d51cece9c5050a6b0e68f0ee0c0676928e99
Dushyant Yadav <dushyant9309@gmail.com>
2023-01-05 01:23:31 +0530

    Pls

--- src/util/axios.js
+++ src/util/axios.js
@@ -1,6 +1,8 @@
 const axios = require("axios");
 const { MAYA_PAC_URL } = require("../constants");
 
+console.log('axios base URL is', MAYA_PAC_URL)
+
 module.exports = axios.create({
     baseURL: MAYA_PAC_URL,
 });
